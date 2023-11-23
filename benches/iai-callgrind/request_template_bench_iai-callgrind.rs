ad5a24e583fff23ac0c1abcb663a914f4faa60ba
alankritdabral_2 <149957793+AlooDon@users.noreply.github.com>
2023-11-23 10:38:06 +0000

    conflicts solveed

--- benches/iai-callgrind/request_template_bench_iai-callgrind.rs
+++ benches/iai-callgrind/request_template_bench_iai-callgrind.rs
@@ -9,56 +9,50 @@ use tailcall::request_template::RequestTemplate;
 
 #[derive(Setters)]
 struct Context {
-    pub value: serde_json::Value,
-    pub headers: HeaderMap,
+  pub value: serde_json::Value,
+  pub headers: HeaderMap,
 }
 
 impl Default for Context {
-    fn default() -> Self {
-        Self { value: serde_json::Value::Null, headers: HeaderMap::new() }
-    }
+  fn default() -> Self {
+    Self { value: serde_json::Value::Null, headers: HeaderMap::new() }
+  }
 }
 
 impl PathString for Context {
-    fn path_string<T: AsRef<str>>(&self, parts: &[T]) -> Option<std::borrow::Cow<'_, str>> {
-        self.value.path_string(parts)
-    }
+  fn path_string<T: AsRef<str>>(&self, parts: &[T]) -> Option<std::borrow::Cow<'_, str>> {
+    self.value.path_string(parts)
+  }
 }
 
 impl HasHeaders for Context {
-    fn headers(&self) -> &HeaderMap {
-        &self.headers
-    }
+  fn headers(&self) -> &HeaderMap {
+    &self.headers
+  }
 }
-
 #[library_benchmark]
 fn benchmark_to_request() {
-    const NUM_ITERATIONS: usize = 10000; // Adjust the number of iterations as needed
+  let tmpl_mustache = RequestTemplate::try_from(Endpoint::new(
+    "http://localhost:3000/{{args.b}}?a={{args.a}}&b={{args.b}}&c={{args.c}}".to_string(),
+  ))
+  .unwrap();
 
-    let tmpl_mustache = RequestTemplate::try_from(Endpoint::new(
-        "http://localhost:3000/{{args.b}}?a={{args.a}}&b={{args.b}}&c={{args.c}}".to_string(),
-    ))
-    .unwrap();
+  let tmpl_literal =
+    RequestTemplate::try_from(Endpoint::new("http://localhost:3000/foo?a=bar&b=foo&c=baz".to_string())).unwrap();
 
-    let tmpl_literal =
-        RequestTemplate::try_from(Endpoint::new("http://localhost:3000/foo?a=bar&b=foo&c=baz".to_string())).unwrap();
+  let ctx = Context::default().value(json!({
+      "args": {
+          "b": "foo"
+      }
+  }));
 
-    let ctx = Context::default().value(json!({
-        "args": {
-            "b": "foo"
-        }
-    }));
-
-    // Introduce a loop for more iterations
-    for _ in 0..NUM_ITERATIONS {
-        black_box(tmpl_literal.to_request(&ctx).unwrap());
-        black_box(tmpl_mustache.to_request(&ctx).unwrap());
-    }
+  // Benchmarks without the criterion framework
+  black_box(tmpl_literal.to_request(&ctx).unwrap());
+  black_box(tmpl_mustache.to_request(&ctx).unwrap());
 }
 
 library_benchmark_group!(
-    name = bench_to_request;
-    benchmarks = benchmark_to_request
-);
+  name= bench_to_request;
+  benchmarks= benchmark_to_request);
 
 main!(library_benchmark_groups = bench_to_request);
