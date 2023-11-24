d345eafdd3a1e7ba261f454ac68575084d4421d2
alankritdabral_2 <149957793+AlooDon@users.noreply.github.com>
2023-11-24 17:35:40 +0530

    Update check_degradation.sh

--- .github/scripts/check_degradation.sh
+++ .github/scripts/check_degradation.sh
@@ -9,7 +9,8 @@ cargo bench --bench impl_path_string_for_evaluation_context -- --save-baseline m
 cargo bench --bench data_loader_bench -- --save-baseline main_branch
 cargo bench --bench request_template_bench -- --save-baseline main_branch
 cargo bench --bench json_like_bench -- --save-baseline main_branch
-        
+
+echo "critcmp main_branch new_branch"
 critcmp main_branch new_branch | awk 'NR>2 {
     item = $1
     before = $7
