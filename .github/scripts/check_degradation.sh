0bb3d2a8dcfeafae86c6e73adc42189c900eb494
alankritdabral_2 <149957793+AlooDon@users.noreply.github.com>
2023-11-24 12:37:24 +0530

    Update check_degradation.sh

--- .github/scripts/check_degradation.sh
+++ .github/scripts/check_degradation.sh
@@ -1,11 +1,14 @@
           current_branch=$(git rev-parse --abbrev-ref HEAD)
           git fetch
+          git stash
           git checkout main
 
           cargo bench --bench impl_path_string_for_evaluation_context -- --save-baseline new_branch
           cargo bench --bench data_loader_bench -- --save-baseline new_branch
           cargo bench --bench request_template_bench -- --save-baseline new_branch
           cargo bench --bench json_like_bench -- --save-baseline new_branch
+          git stash drop 
+          git stash
 
           git checkout "$current_branch"
           cargo bench --bench impl_path_string_for_evaluation_context -- --save-baseline main_branch
