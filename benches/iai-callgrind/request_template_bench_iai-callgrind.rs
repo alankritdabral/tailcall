1634b647a0d94d43d6b037dc7ae0ac9deb691476
alankritdabral_2 <149957793+AlooDon@users.noreply.github.com>
2023-11-27 16:04:51 +0530

    Update request_template_bench_iai-callgrind.rs

--- benches/iai-callgrind/request_template_bench_iai-callgrind.rs
+++ benches/iai-callgrind/request_template_bench_iai-callgrind.rs
@@ -2,21 +2,26 @@ use iai_callgrind::{black_box, library_benchmark, library_benchmark_group, main}
 use serde_json::json;
 use tailcall::benchmark::{create_request_templates, Context};
 
+const NUM_ITERATIONS: usize = 10; 
+
 #[library_benchmark]
 fn benchmark_to_request() {
-  let (tmpl_literal, tmpl_mustache) = create_request_templates();
-  let ctx = Context::default().value(json!({
-      "args": {
-          "b": "foo"
-      }
-  }));
+    let (tmpl_literal, tmpl_mustache) = create_request_templates();
+    let ctx = Context::default().value(json!({
+        "args": {
+            "b": "foo"
+        }
+    }));
 
-  black_box(tmpl_literal.to_request(&ctx).unwrap());
-  black_box(tmpl_mustache.to_request(&ctx).unwrap());
+    for _ in 0..NUM_ITERATIONS {
+        black_box(tmpl_literal.to_request(&ctx).unwrap());
+        black_box(tmpl_mustache.to_request(&ctx).unwrap());
+    }
 }
 
 library_benchmark_group!(
-  name= bench_to_request;
-  benchmarks= benchmark_to_request);
+    name = bench_to_request;
+    benchmarks = benchmark_to_request
+);
 
 main!(library_benchmark_groups = bench_to_request);
