fd81f5de834617e194f474b51499cc52f43bd39b
alankritdabral_2 <149957793+AlooDon@users.noreply.github.com>
2023-11-27 16:04:40 +0530

    Update json_like_bench_iai-callgrind.rs

--- benches/iai-callgrind/json_like_bench_iai-callgrind.rs
+++ benches/iai-callgrind/json_like_bench_iai-callgrind.rs
@@ -2,26 +2,26 @@ use iai_callgrind::{black_box, library_benchmark, library_benchmark_group, main}
 use serde_json::json;
 use tailcall::benchmark::gather_path_matches;
 
+const NUM_ITERATIONS: usize = 10; 
+
 #[library_benchmark]
 fn benchmark_batched_body() {
-  let input = json!({
-      "data": [
-          {"user": {"id": "1"}},
-          {"user": {"id": "2"}},
-          {"user": {"id": "3"}},
-          {"user": [
-              {"id": "4"},
-              {"id": "5"}
-          ]}
-      ]
-  });
+    for _ in 0..NUM_ITERATIONS {
+        let input = json!({
+            "data": [
+                {"user": {"id": "1"}},
+                {"user": {"id": "2"}},
+                {"user": {"id": "3"}},
+                {"user": [
+                    {"id": "4"},
+                    {"id": "5"}
+                ]}
+            ]
+        });
 
-  black_box(gather_path_matches(&input, &["data", "user", "id"]));
+        black_box(gather_path_matches(&input, &["data", "user", "id"]));
+    }
 }
 
-library_benchmark_group!(
-    name = batched_body;
-    benchmarks = benchmark_batched_body
-);
-
+library_benchmark_group!(name = batched_body; benchmarks = benchmark_batched_body);
 main!(library_benchmark_groups = batched_body);
