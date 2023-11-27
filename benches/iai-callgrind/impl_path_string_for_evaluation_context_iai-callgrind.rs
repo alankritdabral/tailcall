49a9e9a8b9deef3f0b9d46f298c7ae90438e01f4
alankritdabral_2 <149957793+AlooDon@users.noreply.github.com>
2023-11-27 16:04:27 +0530

    Update impl_path_string_for_evaluation_context_iai-callgrind.rs

--- benches/iai-callgrind/impl_path_string_for_evaluation_context_iai-callgrind.rs
+++ benches/iai-callgrind/impl_path_string_for_evaluation_context_iai-callgrind.rs
@@ -5,51 +5,50 @@ use tailcall::lambda::EvaluationContext;
 use tailcall::path_string::PathString;
 
 const INPUT_VALUE: &[&[&str]] = &[
-  // existing values
-  &["value", "root"],
-  &["value", "nested", "existing"],
-  // missing values
-  &["value", "missing"],
-  &["value", "nested", "missing"],
+    // existing values
+    &["value", "root"],
+    &["value", "nested", "existing"],
+    // missing values
+    &["value", "missing"],
+    &["value", "nested", "missing"],
 ];
 
 const ARGS_VALUE: &[&[&str]] = &[
-  // existing values
-  &["args", "root"],
-  &["args", "nested", "existing"],
-  // missing values
-  &["args", "missing"],
-  &["args", "nested", "missing"],
+    // existing values
+    &["args", "root"],
+    &["args", "nested", "existing"],
+    // missing values
+    &["args", "missing"],
+    &["args", "nested", "missing"],
 ];
 
 const HEADERS_VALUE: &[&[&str]] = &[&["headers", "existing"], &["headers", "missing"]];
 
 const VARS_VALUE: &[&[&str]] = &[&["vars", "existing"], &["vars", "missing"]];
 
+const NUM_ITERATIONS: usize = 10; 
 #[library_benchmark]
 fn bench_main() {
-  let mut req_ctx = RequestContext::default().req_headers(TEST_HEADERS.clone());
+    for _ in 0..NUM_ITERATIONS {
+        let mut req_ctx = RequestContext::default().req_headers(TEST_HEADERS.clone());
 
-  req_ctx.server.vars = TEST_VARS.clone();
+        req_ctx.server.vars = TEST_VARS.clone();
 
-  let eval_ctx = EvaluationContext::new(&req_ctx, &MockGraphqlContext);
+        let eval_ctx = EvaluationContext::new(&req_ctx, &MockGraphqlContext);
 
-  assert_test(&eval_ctx);
+        assert_test(&eval_ctx);
 
-  let all_inputs = INPUT_VALUE
-    .iter()
-    .chain(ARGS_VALUE)
-    .chain(HEADERS_VALUE)
-    .chain(VARS_VALUE);
+        let all_inputs = INPUT_VALUE
+            .iter()
+            .chain(ARGS_VALUE)
+            .chain(HEADERS_VALUE)
+            .chain(VARS_VALUE);
 
-  for input in all_inputs {
-    let _result = eval_ctx.path_string(input);
-  }
+        for input in all_inputs {
+            let _result = eval_ctx.path_string(input);
+        }
+    }
 }
 
-library_benchmark_group!(
-    name = bench;
-    benchmarks = bench_main
-);
-
+library_benchmark_group!(name = bench; benchmarks = bench_main);
 main!(library_benchmark_groups = bench);
