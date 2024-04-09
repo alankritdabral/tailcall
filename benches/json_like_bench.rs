63b6237522f67d06166861c2a4f63c68f4a993f5
alankritdabral <95607957+alankritdabral@users.noreply.github.com>
2024-04-09 16:30:25 +0530

    Update json_like_bench.rs

--- benches/json_like_bench.rs
+++ benches/json_like_bench.rs
@@ -5,7 +5,7 @@ use tailcall::json::JsonLike;
 fn benchmark_batched_body(c: &mut Criterion) {
     c.bench_function("test_batched_body", |b| {
         b.iter(|| {
-            let input = json!({
+            let mut input = json!({
                 "data": [
                     {"user": {"id": "1"}},
                     {"user": {"id": "2"}},
@@ -18,14 +18,16 @@ fn benchmark_batched_body(c: &mut Criterion) {
                 ]
             });
 
-            black_box(
-                serde_json::to_value(tailcall::json::gather_path_matches(
-                    &input,
-                    &["data".into(), "user".into(), "id".into()],
-                    vec![],
-                ))
-                .unwrap(),
-            );
+            for _ in 0..1000 {
+                black_box(
+                    serde_json::to_value(tailcall::json::gather_path_matches(
+                        &input,
+                        &["data".into(), "user".into(), "id".into()],
+                        vec![],
+                    ))
+                    .unwrap(),
+                );
+            }
         })
     });
 }
@@ -43,14 +45,12 @@ fn benchmark_group_by(c: &mut Criterion) {
     c.bench_function("group_by", |b| {
         b.iter(|| {
             let binding = ["data".into(), "type".into()];
-            black_box(input.group_by(&binding));
+            for _ in 0..1000 {
+                black_box(input.group_by(&binding));
+            }
         })
     });
 }
 
-criterion_group!(
-    benches,
-    benchmark_batched_body,
-    benchmark_group_by
-);
+criterion_group!(benches,benchmark_batched_body,benchmark_group_by);
 criterion_main!(benches);
