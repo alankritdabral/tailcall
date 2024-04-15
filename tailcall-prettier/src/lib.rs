ed679fb6b1f13301053118f57fd1d94f28e948d3
alankritdabral <95607957+alankritdabral@users.noreply.github.com>
2024-04-15 19:06:21 +0530

    Update lib.rs

--- tailcall-prettier/src/lib.rs
+++ tailcall-prettier/src/lib.rs
@@ -45,6 +45,8 @@ pub fn format_with_prettier<T: AsRef<str>>(code: T, file_ty: Parser) -> Result<S
     }
 
     let output = child.wait_with_output()?;
+    println!("Command output: {:?}", output);
+
     if output.status.success() {
         Ok(String::from_utf8(output.stdout)?)
     } else {
